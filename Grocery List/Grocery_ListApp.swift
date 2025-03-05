a8d127d01e3ec7bdf7ece902d116197feced84b7
Harun Nacar <harunnacarr24@gmail.com>
2025-03-05 18:05:52 +0300

    Initial Commit

---
+++ Grocery List/Grocery_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_ListApp.swift
+//  Grocery List
+//
+//  Created by Harun Naçar on 5.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
